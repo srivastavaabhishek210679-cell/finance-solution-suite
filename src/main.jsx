8034a7cd224d322b1afe416bdfe29ac62a161885
Abhishek <abhishek240575@gmail.com>
2026-05-14 11:14:17 +0530

    Fix: Update App import to match lowercase filename

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext.jsx'
-import App from './App.jsx'
+import App from './app.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
